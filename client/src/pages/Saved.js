d87091070fa71af2a19827238f117698ecb79c72
Jared de Uriarte <jared.deuri@gmail.com>
2021-05-11 19:38:34 -0400

    saved books finally loading

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import API from '../utils/API'
 import SavedBooks from '../components/SavedBooks'
@@ -10,47 +10,62 @@ function Saved() {
     const [books, setBooks] = useState([])
 
 
-    const savedBooks = () => {
-        console.log("HELLO ")
-        API.getSavedBooks()
+    // const savedBooks = () => {
+    //     console.log("HELLO ")
+    //     API.getSavedBooks()
+    //         .then(res => {
+    //             console.log("API getSavedBooks", res)
+    //             return res;
+
+    //         })
+    //         .catch(err => console.log(err));
+    // };
+
+    const handleDeleteBook = (id) => {
+        API.deleteBook(id)
             .then(res => {
-                console.log("API getSavedBooks", res)
-                return res; 
-    
+                console.log('Book Deleted!' + id)
             })
             .catch(err => console.log(err));
     };
 
-    const handleDeleteBook = (id) => {
-        API.deleteBook(id)
-        .then (res => {
-            console.log('Book Deleted!' + id)
-        })
-        .catch(err => console.log(err));
+
+    useEffect(() => {
+        loadBooks()
+    }, []);
+
+    function loadBooks() {
+        API.getSavedBooks()
+            .then(res =>
+                setBooks(res.data)
+            )
+            .catch(err => console.log(err));
     };
 
 
-  
 
-    useEffect(() => {
-        if (!books) {
-          return;
-        }
-    
-        // API.getSavedBooks()
-        // .then(res => {
-        //     console.log("API getSavedBooks", res)
-        //   setBooks(res);
 
-        // })
-        // .catch(err => console.log(err));
-        const value = savedBooks(); 
-        console.log("books", value)
-        if ( value !== undefined){
-            setBooks(value);
-        }
+    // useEffect(() => {
+    //     if (!books) {
+    //       return;
+    //     }
 
-      }, [books]);
+    // API.getSavedBooks()
+    // .then(res => {
+    //     console.log("API getSavedBooks", res)
+    //   setBooks(res);
+
+    // })
+    // .catch(err => console.log(err));
+
+
+    //     const value = savedBooks(); 
+    //     console.log("books", value)
+    //     if ( value !== undefined){
+    //         setBooks(value);
+    //     }
+
+    //   }, [books]);
 
 
 
@@ -64,11 +79,11 @@ function Saved() {
                         books.map((book, index) => (
                             <SavedBooks
                                 key={index}
-                                title={book.volumeInfo.title}
-                                authors={book.volumeInfo.authors}
-                                description={book.volumeInfo.description}
-                                imageLink={book.volumeInfo.imageLinks.smallThumbnail}
-                                link={book.volumeInfo.infoLink}
+                                title={book.title}
+                                authors={book.authors}
+                                description={book.description}
+                                image={book.image}
+                                link={book.link}
                                 handleDeleteBook={() => handleDeleteBook(book.id)}
                             />
                         ))
